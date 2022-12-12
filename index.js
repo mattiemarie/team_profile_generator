048c87a4e0ecacf130b592bec977a2d9cee52a6b
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-12 17:34:03 -0500

    adding html structure Manager

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const employeeRole = [
     type: 'list',
     name: 'role',
     message: "What is the employee's role?",
-    choices:['Manager', 'Engineer', 'Intern'],
+    choices:['Manager', 'Engineer', 'Intern', 'Build Team'],
     },
 ];
 
@@ -166,49 +166,10 @@ function internChosen(){
   });
 };
 
-// generateTeam(
-//   //THE HTML GENERATOR WILL GO IN HERE
-//   fs.writeFile('index.html', ,utf)
-// )
 
-//README file formatting
-const generateHTML = ({role, nameManager, idManager, emailManager, officeManager, nameEngineer, idEngineer, emailEngineer, githubEngineer, nameIntern,idIntern, emailIntern, schoolIntern}) =>
-  `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="./style.css" />
-      <title>Team Profile Generator Mock HTML</title>
-  </head>
-  <body>
-      <div class="heading-container" id="heading">
-          My Team
-      </div>
-      <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="employee-title"> ${nameManager,nameEngineer,nameIntern} </h5>
-            <h5 class="employee-position">Employee Position ${role} </h5>
-            <h6 class="employee-id"><span class="id">ID:</span>${idManager,idEngineer,idIntern}</h6>
-            <h6 class="employee-email"><span class="email">Email:</span>${emailManager,emailEngineer,emailIntern}</h6>
-            <h6 class="employee-github"><span class="github">Github:</span>${githubEngineer}</h6>
-            <h6 class="employee-school"><span class="school">School:</span>${schoolIntern}</h6>
-            <h6 class="employee-office"><span class="office">Office:</span>${officeManager}</h6>
-          </div>
-        </div>
-  </body>
-  </html>`;
-
-//Create a function to initialize app
-const init = () => {
-    promptUser()
-        //Write to README.md file
-        .then((answers) => writeFile('indexGenerated.html', generateHTML(answers)))
-        .then(() => console.log('Successfully wrote to indexGenerated.html'))
-        .catch((err) => console.error(err));        
+// Generate Team 
+const generateTeam = () => {
+  fs.writeFileSync(outputPath, generateTeam(newTeamMember), "utf-8")
 };
 
-//Function call to initialize app
-init();
+managerChosen();
