922f8fda64c162a11a6f3bc8a3717368ba31d93d
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-12 18:01:17 -0500

    formatting html

--- generated-html.js
+++ generated-html.js
@@ -1,8 +1,36 @@
-const createdWorkTeam = (workTeam) => {
+const { create } = require("domain");
+
+const createdWorkTeam = workTeam => {
     
     const generateHTML = [];
 
     const generateManagerHTML = ({role, nameManager, idManager, emailManager, officeManager}) =>
+        `<h5 class="employee-title"> ${nameManager} </h5>
+        <h5 class="employee-position">Employee Position ${role} </h5>
+        <h6 class="employee-id"><span class="id">ID:</span>${idManager}</h6>
+        <h6 class="employee-email"><span class="email">Email:</span>${emailManager}</h6>
+        <h6 class="employee-office"><span class="office">Office:</span>${officeManager}</h6>`;
+    html.push(generateManagerHTML);
+
+    const generateEngineerHTML = ({role, nameEngineer, idEngineer, emailEngineer, githubEngineer}) =>
+        `<h5 class="employee-title"> ${nameEngineer} </h5>
+        <h5 class="employee-position">Employee Position ${role} </h5>
+        <h6 class="employee-id"><span class="id">ID:</span>${idEngineer}</h6>
+        <h6 class="employee-email"><span class="email">Email:</span>${emailEngineer}</h6>
+        <h6 class="employee-github"><span class="github">Github:</span>${githubEngineer}</h6>`;
+    html.push(generateEngineerHTML);
+
+    const generateInternHTML = ({role, nameIntern, idIntern, emailIntern, schoolIntern}) =>
+        `<h5 class="employee-title"> ${nameIntern} </h5>
+        <h5 class="employee-position">Employee Position ${role} </h5>
+        <h6 class="employee-id"><span class="id">ID:</span>${idIntern}</h6>
+        <h6 class="employee-email"><span class="email">Email:</span>${emailIntern}</h6>
+        <h6 class="employee-school"><span class="school">School:</span>${schoolIntern}</h6>`;
+    html.push(generateInternHTML);
+};
+
+module.exports = workTeam => {
+    return 
     `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -19,44 +47,9 @@ const createdWorkTeam = (workTeam) => {
         </div>
         <div class="card" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="employee-title"> ${nameManager} </h5>
-              <h5 class="employee-position">Employee Position ${role} </h5>
-              <h6 class="employee-id"><span class="id">ID:</span>${idManager}</h6>
-              <h6 class="employee-email"><span class="email">Email:</span>${emailManager}</h6>
-              <h6 class="employee-office"><span class="office">Office:</span>${officeManager}</h6>
+              ${createdWorkTeam(workTeam)}
             </div>
           </div>
     </body>
     </html>`;
 }
-
-
-
-const generateHTML = ({role, nameManager, idManager, emailManager, officeManager, nameEngineer, idEngineer, emailEngineer, githubEngineer, nameIntern,idIntern, emailIntern, schoolIntern}) =>
-  `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="./style.css" />
-      <title>Team Profile Generator Mock HTML</title>
-  </head>
-  <body>
-      <div class="heading-container" id="heading">
-          My Team
-      </div>
-      <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="employee-title"> ${nameManager,nameEngineer,nameIntern} </h5>
-            <h5 class="employee-position">Employee Position ${role} </h5>
-            <h6 class="employee-id"><span class="id">ID:</span>${idManager,idEngineer,idIntern}</h6>
-            <h6 class="employee-email"><span class="email">Email:</span>${emailManager,emailEngineer,emailIntern}</h6>
-            <h6 class="employee-github"><span class="github">Github:</span>${githubEngineer}</h6>
-            <h6 class="employee-school"><span class="school">School:</span>${schoolIntern}</h6>
-            <h6 class="employee-office"><span class="office">Office:</span>${officeManager}</h6>
-          </div>
-        </div>
-  </body>
-  </html>`;
