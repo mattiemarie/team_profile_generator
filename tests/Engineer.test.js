5f619b6bf98855857eda775c2805af3a46289cb1
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-13 09:22:04 -0500

    Engineer Test Tweaks

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,25 +1,36 @@
 const EngineerRole = require('../roles/Engineer');
 const engineerRole = EngineerRole('chris', '32370', 'coloredinink@gmail.com', 'coloredinink');
 
-it('Constructor Values for the Engineer function are being tested',() => {
-    expect(engineerRole.name).toEqual('chis');
-    expect(engineerRole.id).toEqual('32370');
-    expect(engineerRole.email).toEqual('coloredinink@gmail.com');
-    expect(engineerRole.github).toEqual('coloredinink')
+
+describe("Engineer Role", () => {
+    it('should return input of the constructor of the Engineer Role',() => {
+        expect(engineerRole.name).toEqual('chis');
+        expect(engineerRole.id).toEqual('32370');
+        expect(engineerRole.email).toEqual('coloredinink@gmail.com');
+        expect(engineerRole.github).toEqual('coloredinink')
+    });    
 });
 
-it('getName() method Test',() => {
-    expect(engineerRole.getName()).toEqual('chris');
+describe("getName() Method Test", () => {
+    it('should get the name of the Engineer input',() => {
+        expect(engineerRole.getName()).toEqual('chris');
+    });
 });
 
-it('getId() method Test',() => {
-    expect(engineerRole.getId()).toEqual('32370');
+describe("getID() Method Test", () => {
+    it('should get the id of the Engineer input',() => {
+        expect(engineerRole.getId()).toEqual('32370');
+    });
 });
 
-it('getEmail() method Test',() => {
-    expect(engineerRole.getEmail()).toEqual('coloredinink@gmail.com');
+describe("getEmail() Method Test", () => {
+    it('should get email of the Engineer input',() => {
+        expect(engineerRole.getEmail()).toEqual('coloredinink@gmail.com');
+    });
 });
 
-it('getGithub() method Test', () => {
-    expect(engineerRole.engineerGithub()).toEqual('coloredinink');
+describe("getGithub() Method Test", () => {
+    it('should get the github of the Engineer input', () => {
+        expect(engineerRole.engineerGithub()).toEqual('coloredinink');
+    });
 });
