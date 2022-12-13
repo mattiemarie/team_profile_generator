e3916b80df30696c832fb0ec248ded75ee70eedf
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-13 09:04:22 -0500

    Manager Test Tweaks

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,25 +1,35 @@
 const ManagerRole = require('../roles/Manager');
 const managerRole = ManagerRole('natalie', '32372', 'rebelred@gmail.com', '222');
 
-it('Constructor Values for the Employee function are being tested',() => {
-    expect(managerRole.name).toEqual('natalie');
-    expect(managerRole.id).toEqual('32372');
-    expect(managerRole.email).toEqual('rebelred@gmail.com');
-    expect(managerRole.office).toEqual('222')
+describe("Manager Role", () => {
+    it('should return input of constructor of the Manager Role',() => {
+        expect(managerRole.name).toEqual('natalie');
+        expect(managerRole.id).toEqual('32372');
+        expect(managerRole.email).toEqual('rebelred@gmail.com');
+        expect(managerRole.office).toEqual('222')
+    });
 });
 
-it('getName() method Test',() => {
-    expect(managerRole.getName()).toEqual('natalie');
+describe("getName() Method Test", () => {
+    it('should get the name of the Manager input',() => {
+        expect(managerRole.getName()).toEqual('natalie');
+    });
 });
 
-it('getId() method Test',() => {
-    expect(managerRole.getId()).toEqual('32372');
+describe("getID() Method Test", () => {
+    it('should get the id of the Manager input',() => {
+        expect(managerRole.getId()).toEqual('32372');
+    });
 });
 
-it('getEmail() method Test',() => {
-    expect(managerRole.getEmail()).toEqual('rebelred@gmail.com');
+describe("getEmail() Method Test", () => {
+    it('should get the email of the Manager input',() => {
+        expect(managerRole.getEmail()).toEqual('rebelred@gmail.com');
+    });
 });
 
-it('getGithub() method Test', () => {
-    expect(managerRole.managerOffice()).toEqual('222');
+describe("getOffice() Method Test", () => {
+    it('should get the github of the Manager input', () => {
+        expect(managerRole.managerOffice()).toEqual('222');
+    });
 });
