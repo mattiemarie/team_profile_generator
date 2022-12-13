67bdbb5bcdb492adf82dd3cdb3f67ce90a025cb0
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-13 09:12:21 -0500

    Employee Test Tweaks

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,20 +1,28 @@
 const EmployeeRole = require('../roles/Employee');
 const employeeRole = EmployeeRole('brian', '32369', 'mrgreenside@gmail.com');
 
-it('Constructor Values for the Employee function are being tested',() => {
-    expect(employeeRole.name).toEqual('brian');
-    expect(employeeRole.id).toEqual('32369');
-    expect(employeeRole.email).toEqual('mrgreenside@gmail.com');
+describe("Employee Role", () => {
+    it('should return input of constructor of the Employee Role',() => {
+        expect(employeeRole.name).toEqual('brian');
+        expect(employeeRole.id).toEqual('32369');
+        expect(employeeRole.email).toEqual('mrgreenside@gmail.com');
+    });
 });
 
-it('getName() method Test',() => {
-    expect(employeeRole.getName()).toEqual('brian');
+describe("getName() Method Test", () => {
+    it('should get the name of the Employee input',() => {
+        expect(employeeRole.getName()).toEqual('brian');
+    });
 });
 
-it('getId() method Test',() => {
-    expect(employeeRole.getId()).toEqual('32369');
+describe("getID() Method Test", () => {
+    it('should get the id of the Employee input',() => {
+        expect(employeeRole.getId()).toEqual('32369');
+    });
 });
 
-it('getEmail() method Test',() => {
-    expect(employeeRole.getEmail()).toEqual('mrgreenside@gmail.com');
+describe("getEmail() Method Test", () => {
+    it('should get the email of the Employee Input',() => {
+        expect(employeeRole.getEmail()).toEqual('mrgreenside@gmail.com');
+    }); 
 });
