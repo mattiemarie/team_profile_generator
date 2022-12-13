208c5abd281b318ff2bb1881d47613e561386a52
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-13 09:16:59 -0500

    Intern Test Tweaks

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,25 +1,36 @@
 const InternRole = require('../roles/Intern');
 const internRole = InternRole('luke', '32371', 'prettyblueeyes@gmail.com', 'annadale');
 
-it('Constructor Values for the Intern function are being tested',() => {
-    expect(internRole.name).toEqual('luke');
-    expect(internRole.id).toEqual('32371');
-    expect(internRole.email).toEqual('prettyblueeyes@gmail.com');
-    expect(internRole.school).toEqual('annadale')
+
+describe("Intern Role", () => {
+    it('should return input of constructor of the Intern Role',() => {
+        expect(internRole.name).toEqual('luke');
+        expect(internRole.id).toEqual('32371');
+        expect(internRole.email).toEqual('prettyblueeyes@gmail.com');
+        expect(internRole.school).toEqual('annadale')
+    });    
 });
 
-it('getName() method Test',() => {
-    expect(internRole.getName()).toEqual('luke');
+describe("getName() Method Test", () => {
+    it('should get the name of the Intern input',() => {
+        expect(internRole.getName()).toEqual('luke');
+    });
 });
 
-it('getId() method Test',() => {
-    expect(internRole.getId()).toEqual('32371');
+describe("getID() Method Test", () => {
+    it('should get the id of the Intern input',() => {
+        expect(internRole.getId()).toEqual('32371');
+    });
 });
 
-it('getEmail() method Test',() => {
-    expect(internRole.getEmail()).toEqual('prettyblueeyes@gmail.com');
+describe("getEmail() Method Test", () => {
+    it('should get email of the Intern input',() => {
+        expect(internRole.getEmail()).toEqual('prettyblueeyes@gmail.com');
+    });
 });
 
-it('getSchool() method Test', () => {
-    expect(internRole.internSchool()).toEqual('annadale');
+describe("getSchool() Method Test", () => {
+    it('should get the school of the Intern input', () => {
+        expect(internRole.internSchool()).toEqual('annadale');
+    });
 });
